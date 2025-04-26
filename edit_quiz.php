368fb498f6b57c492f6e6e96116ea310a29cae12
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:22:17 +0300

    Updating the quiz title

--- edit_quiz.php
+++ edit_quiz.php
@@ -7,6 +7,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $quiz_id = intval($_POST['id']);
     $new_title = mysqli_real_escape_string($conn, $_POST['title']);
 
+    $sql = "UPDATE quizzes SET title = '$new_title' WHERE id = $quiz_id";
+
 
 }
 
