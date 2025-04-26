4f42080e70fee259b67d8aff9dff177cba1a3c8f
Mira E <miraelhajj0@gmail.com>
2025-04-26 19:07:37 +0300

    Verifying password

--- login.php
+++ login.php
@@ -13,6 +13,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         $user = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+        }
+        
     }
 
 }
