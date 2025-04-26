9e026f28a400b755c9834ef5b833f37b31b28af0
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:12:00 +0300

    Escaping the title and created_by values

--- create_quiz.php
+++ create_quiz.php
@@ -5,6 +5,11 @@ include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $created_by = intval($_POST['created_by']);
+
+
 }
 
 ?>
