0eecd705ff850e85912042a5dcbfdeb317f417dd
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:29:20 +0300

    Closing database connection

--- create_question.php
+++ create_question.php
@@ -6,5 +6,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $sql = "INSERT INTO questions (quiz_id, question_text, correct_answer) VALUES ($quiz_id, '$question_text', '$correct_answer')";
 
+    mysqli_close($conn);
 }
 ?>
