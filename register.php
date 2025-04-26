9df6bb24252a4ca09ded796f5adaa1ba6e0f7f1e
Mira E <miraelhajj0@gmail.com>
2025-04-26 18:40:53 +0300

    Checking if form was submitted using the POST method

--- register.php
+++ register.php
@@ -2,26 +2,29 @@
 
 include 'connect.php';  
 
-$username = mysqli_real_escape_string($conn, $_POST['username']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
-$confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
 
 
-if ($password !== $confirm_password) {
-    echo "Passwords do not match!";
-    exit;
-}
+    if ($password !== $confirm_password) {
+        echo "Passwords do not match!";
+        exit;
+    }
 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 
-$check_user_sql = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
-$result = mysqli_query($conn, $check_user_sql);
+    $check_user_sql = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
+    $result = mysqli_query($conn, $check_user_sql);
 
-if (mysqli_num_rows($result) > 0) {
-    echo "Username or Email already taken!";
-    exit;
+    if (mysqli_num_rows($result) > 0) {
+        echo "Username or Email already taken!";
+        exit;
+    }
 }
 
 ?>
