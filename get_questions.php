f7ae0d3a32d6ef317fec86fd83c3792f6e78451a
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:34:22 +0300

    Fixing - escaping quiz_id

--- get_questions.php
+++ get_questions.php
@@ -4,6 +4,8 @@ include 'connect.php';
 
 if (isset($_GET['quiz_id'])) {
 
+    $quiz_id = intval($_GET['quiz_id']);
+    
     $sql = "SELECT * FROM questions WHERE quiz_id = $quiz_id";
     $result = mysqli_query($conn, $sql);
 
