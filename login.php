10d65d1894bf1d39475f24b00efc305c89082314
Mira E <miraelhajj0@gmail.com>
2025-04-26 19:06:11 +0300

    Checking if the user exists

--- login.php
+++ login.php
@@ -9,6 +9,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
 
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+    }
+
 }
 
 ?>
