f1f301da3a4c071b9cbd0fe729e258f4ea580e9a
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:27:01 +0300

    Fixing bug (escaping input)

--- delete_quiz.php
+++ delete_quiz.php
@@ -4,6 +4,7 @@ include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    $quiz_id = intval($_POST['id']);
     $sql = "DELETE FROM quizzes WHERE id = $quiz_id";
 
     mysqli_close($conn);
