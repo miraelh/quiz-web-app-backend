6020e2c3126d963a752b4cba3349fda2d3911af4
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:21:56 +0300

    Escaping input values

--- edit_quiz.php
+++ edit_quiz.php
@@ -4,7 +4,10 @@ include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    
+    $quiz_id = intval($_POST['id']);
+    $new_title = mysqli_real_escape_string($conn, $_POST['title']);
+
+
 }
 
 ?>
