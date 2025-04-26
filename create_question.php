8bd78c61e2f28516d1078389da242160e0bafbe1
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:28:57 +0300

    Inserting the question into questions table

--- create_question.php
+++ create_question.php
@@ -4,6 +4,7 @@ include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    $sql = "INSERT INTO questions (quiz_id, question_text, correct_answer) VALUES ($quiz_id, '$question_text', '$correct_answer')";
 
 }
 ?>
