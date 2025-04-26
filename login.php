49375804e61f4fbf3585a1f59c653d628a90627e
Mira E <miraelhajj0@gmail.com>
2025-04-26 19:05:26 +0300

    Retrieving username and password

--- login.php
+++ login.php
@@ -5,6 +5,9 @@ include 'connect.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") { 
 
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
 
 }
 
