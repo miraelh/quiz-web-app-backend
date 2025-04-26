258ff5a5563cfe356fa9461e1e5269315e496a7f
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:37:23 +0300

    Escaping input values

--- edit_question.php
+++ edit_question.php
@@ -4,6 +4,11 @@ include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+
+    $question_id = intval($_POST['id']);
+    $new_question_text = mysqli_real_escape_string($conn, $_POST['question_text']);
+    $new_correct_answer = mysqli_real_escape_string($conn, $_POST['correct_answer']);
+
 }
 
 ?>
