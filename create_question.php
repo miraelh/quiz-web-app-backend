62aa4b3deeb2c500945e51b21a41904deb0fe6be
Mira E <miraelhajj0@gmail.com>
2025-04-26 20:29:42 +0300

    Fixing - Escaping input values

--- create_question.php
+++ create_question.php
@@ -3,6 +3,10 @@
 include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   
+    $quiz_id = intval($_POST['quiz_id']);
+    $question_text = mysqli_real_escape_string($conn, $_POST['question_text']);
+    $correct_answer = mysqli_real_escape_string($conn, $_POST['correct_answer']);
 
     $sql = "INSERT INTO questions (quiz_id, question_text, correct_answer) VALUES ($quiz_id, '$question_text', '$correct_answer')";
 
