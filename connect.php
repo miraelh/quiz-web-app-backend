c003a596c7497e6ec7b57663fbc1765a0d965bd8
Mira E <miraelhajj0@gmail.com>
2025-04-27 23:23:54 +0300

    Fixing syntax error

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $username = 'root';
 $password = ''; 
 $dbname = 'quizmii';
 
-$conn = mysqli_connect($host, $username,$password, $dbname)
+$conn = mysqli_connect($host, $username,$password, $dbname);
 
 if(!$conn) {
     die ("Connection failed" . mysqli_connect_error())
